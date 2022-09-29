5e0ad863a9f2caab7ce2763e80bb7a75913d2001
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2022-09-28 23:33:45 -0700

    :zap: resource example using timer

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,14 @@ fn add_people(mut commands: Commands) {
         .insert(Name("Zayna Nieves".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
+struct GreetTimer(Timer);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
@@ -31,14 +36,10 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people)
-            .add_system(hello_world)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
             .add_system(greet_people);
     }
-}
-
-fn hello_world() {
-    println!("hello world!");
 }
 
 fn main() {
